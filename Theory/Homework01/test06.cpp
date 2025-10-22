e6a1ce00c467f677bdccadb5eb5ac63009a117b4
梁浩天 <l15304704058@126.com>
2025-10-22 13:12:46 +0800

    feat: 添加链表创建和合并功能
    - 实现了创建有序链表的函数 CreateList
    - 添加了合并两个有序链表的函数 MergeList
    - 增加了打印链表的函数 PrintLink
    - 在 main 函数中测试了链表的创建和合并功能

--- Theory/Homework01/test06.cpp
+++ Theory/Homework01/test06.cpp
@@ -18,7 +18,7 @@ void InitLNode(LinkList &L){
     L->next = nullptr;
 }
 
-void CreatList(LinkList &L,int arr[],int n){
+void CreateList(LinkList &L,int arr[],int n){
     InitLNode(L);
     LNode *tail = L;
     for(int i = 0;i<n;i++){
@@ -29,8 +29,60 @@ void CreatList(LinkList &L,int arr[],int n){
     }
     tail->next = nullptr;
 }
-int main() {
 
+void MergeList(LinkList &La,LinkList &Lb,LinkList &Lc){
+    LNode *pa = La->next;
+    LNode *pb = Lb-> next;
+
+    Lc = La;
+    LNode *pc = Lc;
+
+    while (pa && pb) {
+        //La中的元素比Lb中元素小，那么Lc指向的就是较小的元素
+        if(pa ->data <= pb ->data){
+            pc->next = pa;
+            pc = pa;
+            pa = pa->next;
+        }else{
+            pc ->next = pb;
+            pc = pb;
+            pb = pb->next;
+        }
+    }
+    pc->next = pa? pa:pb;
+    delete Lb;
+}
+
+//打印链表
+void PrintLink(LinkList &L){
+    LNode *p = L->next;
+    while(p){
+        cout << p->data << "\t";
+        p = p->next;
+    }
+    cout  << endl;
+    
+}
+int main() {
+    LinkList La, Lb, Lc;
+    
+    // 创建有序链表A
+    int arrA[] = {1, 3, 5, 7, 9};
+    CreateList(La, arrA, 5);
+    cout << "链表A: ";
+    PrintLink(La);
+    
+    // 创建有序链表B
+    int arrB[] = {2, 4, 6, 8, 10};
+    CreateList(Lb, arrB, 5);
+    cout << "链表B: ";
+    PrintLink(Lb);
+    
+    // 合并链表
+    MergeList(La, Lb, Lc);
+    cout << "合并后链表C: ";
+    PrintLink(Lc);
+    
 
     cout << "按回车键退出..." << endl;
     cin.get();
