0a95731491f2fa5ef104de6b60af6c029e1c450e
梁浩天 <l15304704058@126.com>
2025-10-22 18:44:07 +0800

    ✨ feat: 添加回文判断功能
    - 实现了判断以‘@’为结束符的字符序列是否为回文的算法
    - 使用栈结构存储字符并进行比较
    - 提供了完整的主函数进行测试

---
+++ Theory/Homework01/test10.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// 功能描述：
+// 称正读和反读都相同的字符序列为“回文”,
+// 例如, "abeddcba” "qwerewq”是回文，“ashgash”不是回文。
+// 试写一个算法判断读入的一个以‘@’为结束符的字符序列是否为回文。
+
+bool isPalindrome(){
+    stack<char> s;
+    string str = "";
+    char ch;
+
+    cout << "请输入字符（以@结尾）" << endl;
+    
+    while (cin>>ch &&ch != '@') {
+        str+=ch;
+        s.push(ch);
+    }
+
+    for(int i = 0;i<str.length();i++){
+        if(str[i] != s.top()){
+            return false;
+        }
+        s.pop();
+    }
+    return true;
+}
+
+
+int main() {
+    if (isPalindrome()) {
+        cout << "是回文！" << endl;
+    } else {
+        cout << "不是回文！" << endl;
+    }
+
+    cout << "按回车键退出..." << endl;
+    cin.get();
+    cin.get();
+    return 0;
+}
